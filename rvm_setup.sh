c86bbad62dfb3c11053f5fb31f5cc38a798bcf1d
Gius. Camerlingo <gcamerli@protonmail.com>
2018-03-19 16:50:13 +0100

    Clean and update script

--- rvm_setup.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-# ./rvm_setup.sh
-
-# Install ruby version
-rvm install 2.3.6
--- script/run.sh
+++ script/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # ./run.sh
 
-docker run -it -h gitlab.example.com -p 3000:3000 --name gitlab-ce gitlab-ce zsh
+docker run -it -h gitlab.example.com -p 3000:3000 -p 4000:22 --name gitlab-ce gitlab-ce zsh
 
 # Test in a web browser
 #
