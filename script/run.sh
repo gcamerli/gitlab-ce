192231205071d62ec0606358113a9e07cb5e763d
Gius. Camerlingo <gcamerli@protonmail.com>
2018-03-22 17:21:22 +0100

    Clean and update run script

--- script/run.sh
+++ script/run.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 # ./run.sh
 
-docker run -it -h gitlab.example.com -p 3000:3000 -p 4000:22 --name gitlab-ce gitlab-ce zsh
+docker run -d -h gitlab.example.com \
+	-p 3000:3000 -p 2222:22 \
+    --restart always \
+    --volume /srv/gitlab/config:/etc/gitlab \
+    --volume /srv/gitlab/logs:/var/log/gitlab \
+    --volume /srv/gitlab/data:/var/opt/gitlab \
+    -e GITLAB_OMNIBUS_CONFIG="external_url 'http://gitlab.example.com:3000'; gitlab_rails['gitlab_shell_ssh_port']=2222;" \
+	--name gitlab gitlab-ce
 
 # Test in a web browser
 #
-# Access to <http://gitlab.example.com:3000>
+# Access to <http://localhost:3000>
 #
 # login: root
 # pwd: 5iveL!fe
